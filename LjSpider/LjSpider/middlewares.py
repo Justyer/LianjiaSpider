56ffb14779b2ed2c18d0847533c73218d8ca122d
Justyer <1554694323@qq.com>
2017-10-25 15:35:54 +0800

    hehe

--- LjSpider/LjSpider/middlewares.py
+++ LjSpider/LjSpider/middlewares.py
@@ -25,7 +25,7 @@ class ProxyMiddleware(object):
             url = 'http://api.ip.data5u.com/dynamic/get.html?order=82fdbca90fb003889c47a3bfc3d5897c&sep=5'
             proxy_url = requests.get(url=url,timeout=2).text.split(',')[0]
             print 'proxyUrl:', proxy_url
-            if proxy_url != 'too many requests':
+            if proxy_url != 'too many requests' and proxy_url[0] != '{':
                 request.meta['proxy'] = "http://" + proxy_url
         except Exception, e:
             pass
